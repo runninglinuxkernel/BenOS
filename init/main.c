2e241876d4cd320acf2bc8d668d134fe8ea4ae7f
benshushu <runninglinuxkernel@126.com>
2020-04-17 21:52:18 -0700

    add mini_uart and pl_uart support for Pi
    Signed-off-by: benshushu <runninglinuxkernel@126.com>

--- init/main.c
+++ init/main.c
@@ -3,7 +3,8 @@
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
+	uart_send_string("=Welcome BenOS!\r\n");
+	uart_send_string("i am benshushu!\r\n");
 
 	while (1) {
 		uart_send(uart_recv());
