6205f98679257370986d62404d6563da008c825d
benshushu <runninglinuxkernel@126.com>
2020-05-14 17:57:42 -0700

    using readl/writel
    Signed-off-by: benshushu <runninglinuxkernel@126.com>

--- arch/arm64/mach-rpi/mini_uart.c
+++ arch/arm64/mach-rpi/mini_uart.c
@@ -1,53 +1,64 @@
 #include <mach/mini_uart.h>
 #include <mach/gpio.h>
-#include <asm/utils.h>
+#include <asm/io.h>
 
-void uart_send(char c) {
-  while (1) {
-    if (get32(AUX_MU_LSR_REG) & 0x20)
-      break;
-  }
-  put32(AUX_MU_IO_REG, c);
+void uart_send ( char c )
+{
+	while(1) {
+		if(readl(AUX_MU_LSR_REG)&0x20)
+			break;
+	}
+	writel(c, AUX_MU_IO_REG);
 }
 
-char uart_recv(void) {
-  while (1) {
-    if (get32(AUX_MU_LSR_REG) & 0x01)
-      break;
-  }
-  return (get32(AUX_MU_IO_REG) & 0xFF);
+char uart_recv ( void )
+{
+	while(1) {
+		if(readl(AUX_MU_LSR_REG)&0x01)
+			break;
+	}
+	return(readl(AUX_MU_IO_REG)&0xFF);
 }
 
-void uart_send_string(char *str) {
-  for (int i = 0; str[i] != '\0'; i++) {
-    uart_send((char)str[i]);
-  }
+void uart_send_string(char* str)
+{
+	for (int i = 0; str[i] != '\0'; i ++) {
+		uart_send((char)str[i]); }
 }
 
-void uart_init(void) {
-  unsigned int selector;
+void dbg_puts(char *s)
+{
+	while (*s != '\0') {
+		while(!(readl(AUX_MU_LSR_REG) & 0x20))
+			;
+		writel(*s, AUX_MU_IO_REG);
+		s++;
+	}
+}
 
-  selector = get32(GPFSEL1);
-  selector &= ~(7 << 12); // clean gpio14
-  selector |= 2 << 12;    // set alt5 for gpio14
-  selector &= ~(7 << 15); // clean gpio15
-  selector |= 2 << 15;    // set alt5 for gpio15
-  put32(GPFSEL1, selector);
+void uart_init ( void )
+{
+	unsigned int selector;
 
-  put32(GPPUD, 0);
-  delay(150);
-  put32(GPPUDCLK0, (1 << 14) | (1 << 15));
-  delay(150);
-  put32(GPPUDCLK0, 0);
+	selector = readl(GPFSEL1);
+	selector &= ~(7<<12);                   // clean gpio14
+	selector |= 2<<12;                      // set alt5 for gpio14
+	selector &= ~(7<<15);                   // clean gpio15
+	selector |= 2<<15;                      // set alt5 for gpio15
+	writel(selector, GPFSEL1);
 
-  put32(AUX_ENABLES,
-        1); // Enable mini uart (this also enables access to it registers)
-  put32(AUX_MU_CNTL_REG, 0); // Disable auto flow control and disable receiver
-                             // and transmitter (for now)
-  put32(AUX_MU_IER_REG, 0);    // Disable receive and transmit interrupts
-  put32(AUX_MU_LCR_REG, 3);    // Enable 8 bit mode
-  put32(AUX_MU_MCR_REG, 0);    // Set RTS line to be always high
-  put32(AUX_MU_BAUD_REG, 270); // Set baud rate to 115200
+	writel(0, GPPUD);
+	delay(150);
+	writel((1<<14)|(1<<15), GPPUDCLK0);
+	delay(150);
+	writel(0, GPPUDCLK0);
 
-  put32(AUX_MU_CNTL_REG, 3); // Finally, enable transmitter and receiver
+	writel(1, AUX_ENABLES);                   //Enable mini uart (this also enables access to it registers)
+	writel(0, AUX_MU_CNTL_REG);               //Disable auto flow control and disable receiver and transmitter (for now)
+	writel(0, AUX_MU_IER_REG);                //Disable receive and transmit interrupts
+	writel(3, AUX_MU_LCR_REG);                //Enable 8 bit mode
+	writel(0, AUX_MU_MCR_REG);                //Set RTS line to be always high
+	writel(270, AUX_MU_BAUD_REG);             //Set baud rate to 115200
+
+	writel(3, AUX_MU_CNTL_REG);               //Finally, enable transmitter and receiver
 }
--- arch/arm64/mach-rpi/pl_uart.c
+++ arch/arm64/mach-rpi/pl_uart.c
@@ -1,26 +1,24 @@
-#include <asm/utils.h>
 #include <mach/uart.h>
 #include <mach/gpio.h>
+#include <asm/io.h>
 
 void uart_send (char c)
 {
 	// wait for transmit FIFO to have an available slot
-	while(get32(U_FR_REG) & (1<<5)) { }
+	while(readl(U_FR_REG) & (1<<5)) { }
 
-	put32(U_DATA_REG, c);
+	writel(c, U_DATA_REG);
 }
 
 char uart_recv ()
 {
 	// wait for receive FIFO to have data to read
-	while(get32(U_FR_REG) & (1<<4)) { }
+	while(readl(U_FR_REG) & (1<<4)) { }
 
-	return(get32(U_DATA_REG) & 0xFF);
+	return(readl(U_DATA_REG) & 0xFF);
 }
 
-void uart_send_string(char* str)
-{
-	for (int i = 0; str[i] != '\0'; i ++) {
+void uart_send_string(char* str) { for (int i = 0; str[i] != '\0'; i ++) {
 		uart_send((char) str[i]);
 	}
 }
@@ -29,21 +27,20 @@ void uart_init (void)
 {
 	unsigned int selector;
 
-	selector = get32(GPFSEL1);
-	selector &= ~(7<<12);                   // clean gpio14
+	selector = readl(GPFSEL1); selector &= ~(7<<12);                   // clean gpio14
 	selector |= 4<<12;                      // set alt0 for gpio14
 	selector &= ~(7<<15);                   // clean gpio15
 	selector |= 4<<15;                      // set alt0 for gpio 15
-	put32(GPFSEL1, selector);
+	writel(selector, GPFSEL1);
 
-	put32(GPPUD, 0);
+	writel(0, GPPUD);
 	delay(150);
-	put32(GPPUDCLK0, (1<<14) | (1<<15));
+	writel((1<<14) | (1<<15), GPPUDCLK0);
 	delay(150);
-	put32(GPPUDCLK0, 0);
+	writel(0, GPPUDCLK0);
 
     
-	put32(U_CR_REG, 0);                    // disable UART until configuration is done
+	writel(0, U_CR_REG);                    // disable UART until configuration is done
 
 	/* baud divisor = UARTCLK / (16 * baud_rate) 
 	                = 48 * 10^6 / (16 * 115200) = 26.0416666667
@@ -54,10 +51,10 @@ void uart_init (void)
        generated baud rate = (48 * 10^6) / (16 * 26.046875) = 115177
 	   error = |(115177 - 115200) / 115200 * 100| = 0.02%
 	*/
-	put32(U_IBRD_REG, 26);                 // baud rate divisor, integer part
-	put32(U_FBRD_REG, 3);                  // baud rate divisor, fractional part
+	writel(26, U_IBRD_REG);                 // baud rate divisor, integer part
+	writel(3, U_FBRD_REG);                  // baud rate divisor, fractional part
 	
-	put32(U_LCRH_REG, (1<<4) | (3<<5));    // enable FIFOs and 8 bits frames
-	put32(U_IMSC_REG, 0);                  // mask interupts
-	put32(U_CR_REG, 1 | (1<<8) | (1<<9));  // enable UART, receive and transmit
+	writel((1<<4) | (3<<5), U_LCRH_REG);    // enable FIFOs and 8 bits frames
+	writel(0, U_IMSC_REG);                  // mask interupts
+	writel(1 | (1<<8) | (1<<9), U_CR_REG);  // enable UART, receive and transmit
 }
