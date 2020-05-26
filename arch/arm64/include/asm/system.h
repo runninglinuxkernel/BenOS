685076ff37e934af14d297f66192cf9e7861ad16
benshushu <runninglinuxkernel@126.com>
2020-05-26 06:34:38 -0700

    add mem_init() support
    show the totoal memory info in bootup.
    
    Signed-off-by: benshushu <runninglinuxkernel@126.com>

--- arch/arm64/include/asm/system.h
+++ arch/arm64/include/asm/system.h
@@ -28,19 +28,3 @@ struct pt_regs {
 	u64 unused;
 	u64 stackframe[2];
 };
-
-struct cpu_context {
-	unsigned long x19;
-	unsigned long x20;
-	unsigned long x21;
-	unsigned long x22;
-	unsigned long x23;
-	unsigned long x24;
-	unsigned long x25;
-	unsigned long x26;
-	unsigned long x27;
-	unsigned long x28;
-	unsigned long fp;
-	unsigned long sp;
-	unsigned long pc;
-};
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1,12 +1,28 @@
 #include <asm/mm.h>
+#include <printk.h>
 
 #define NR_PAGES (TOTAL_MEMORY / PAGE_SIZE)
 
 static unsigned short mem_map[NR_PAGES] = {0,};
 
+static unsigned long phy_start_address;
+
 void mem_init(unsigned long start_mem, unsigned long end_mem)
 {
+	unsigned long nr_free_pages = 0;
+	unsigned long free;
 
+	start_mem = PAGE_ALIGN(start_mem);
+	phy_start_address = start_mem;
+	end_mem &= PAGE_MASK;
+	free = end_mem - start_mem;
+
+	while (start_mem < end_mem) {
+		nr_free_pages++;
+		start_mem += PAGE_SIZE;
+	}
+
+	printk("Memory: %uKB available, %u free pages\n", free/1024, nr_free_pages);
 }
 
 unsigned long get_free_page(void)
